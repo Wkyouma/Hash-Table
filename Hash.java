c0cd3e6e19524f61b71bd7bc37475339cb894a38
Igor Terplak Gutierrez <135295319+Wkyouma@users.noreply.github.com>
2024-10-18 22:51:11 -0300

    Update Hash.java

--- Hash.java
+++ Hash.java
@@ -1,27 +1,19 @@
-
-
-class No {
-    int chave;
-    No proximo;
-
-    public No(int chave) {
-        this.chave = chave;
-        this.proximo = null;
-    }
-}
+import java.util.Random;
 
 public class Hash {
-    No[] tabela1;
-    No[] tabela2;
-    No[] tabela3;
-    int tamanhoTabela1 = 10;
+    private Random random;
+    Node[] tabela1;
+    Node[] tabela2;
+    Node[] tabela3;
+    int tamanhoTabela1 = 100000;
     int tamanhoTabela2 = 10000;
     int tamanhoTabela3 = 1000;
-
-    public Hash() {
-        tabela1 = new No[tamanhoTabela1];
-        tabela2 = new No[tamanhoTabela2];
-        tabela3 = new No[tamanhoTabela3];
+    
+    public Hash(long seed) {
+        this.random = new Random(seed);
+        tabela1 = new Node[tamanhoTabela1];
+        tabela2 = new Node[tamanhoTabela2];
+        tabela3 = new Node[tamanhoTabela3];
     }
 
     private int h(int tipo,int chave, int tamanhoTabela) { 
@@ -33,30 +25,18 @@ public class Hash {
             return (int) (tamanhoTabela * valorFracionario);
         }
         else if(tipo==3){
-            //inserir terceiro tipo de hash
+            //implementar terceiro tipo de hash
         }
         return -1; 
     }
 
    
 
-    public void inserir(int tipo,int chave, No[] tabela, int tamanhoTabela) {
-        int i = h(tipo,chave, tamanhoTabela);
-        No novo = new No(chave);
-        if (tabela[i] == null) {
-            tabela[i] = novo;
-        } else { 
-            No atual = tabela[i];
-            while (atual.proximo != null) {
-                atual = atual.proximo;
-            }
-            atual.proximo = novo;
-        }
-    }
 
-    public void exibirTabela(No[] tabela, int tamanhoTabela) {
+    public void exibirTabela(Node[] tabela, int tamanhoTabela) {
         for (int i = 0; i < tamanhoTabela; i++) {
-            No atual = tabela[i];
+            Node atual = tabela[i];
+            System.out.println("----------------------------------------------------------------");
             System.out.print("Posição " + i + ":");
             while (atual != null) {
                 System.out.print("-> "+atual.chave);
@@ -65,18 +45,46 @@ public class Hash {
             System.out.println("");
         }
     }
+    public void buscar(Node[] tabela, int chave, int tipo, int tamanhoTabela){
+       //implementar
+    }
 
-    public void inserirVarios( int tipo,int numero, No[] tabela, int tamanhoTabela) {
+
+    public void inserirVarios(int tipoH, int tipoR, int numero, Node[] tabela, int tamanhoTabela) {
         for (int i = 0; i < numero; i++) {
-            int chave = (int) (Math.random() * 1000000) + 1;
-            inserir(tipo,chave, tabela, tamanhoTabela);
+            int chave = random.nextInt(100000000) + 1;
+            int a = h(tipoH, chave, tamanhoTabela);
+            
+            Node novo = new Node(chave);
+    
+            if (tipoR == 1) { // Encadeamento
+                if (tabela[a] == null) {
+                    tabela[a] = novo;
+                } else { 
+                    Node atual = tabela[a];
+                    while (atual.proximo != null) {
+                        atual = atual.proximo;
+                    }
+                    atual.proximo = novo;
+                }
+            } else if (tipoR == 2) {// rehasing simples
+                while (tabela[a] != null) {
+                    a = (a + 1) % tamanhoTabela;
+                }
+                tabela[a] = novo;
+            }
         }
     }
 
     public static void main(String[] args) {
-        Hash hash = new Hash();
+        Hash hash = new Hash(21);//inserir semente para testes
 
-        hash.inserirVarios(1 , 1000, hash.tabela3, hash.tamanhoTabela3); 
-        hash.exibirTabela(hash.tabela3, hash.tamanhoTabela3); 
+        hash.inserirVarios(1 ,2, 9000, hash.tabela2, hash.tamanhoTabela2); 
+        hash.exibirTabela(hash.tabela2, hash.tamanhoTabela2); 
+        //tipoH é o tipo de hase que deseja 1 para divisão, 2 para multiplicação e 3 para...
+        //tipoR é o tipo de tratamento de colisão que deseja 1 para encadeamento e 2 para rehashing linear
+        //numero é o número de elementos a serem inseridos
+        //tabela é a tabela hash que deseja manipular
+        //tamanhoTabela é Tamanho da tabela 1 para 100000 2 para 10000 e 3 para 1000
     }
 }
